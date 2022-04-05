e87c18f9ae5add66fb588456dd4bea04a9f2e27f
Alawon1 <alawon12@gmail.com>
2022-04-05 11:54:00 +0200

    sample test exam n.1 without last task

--- SampleExams/main.py
+++ SampleExams/main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Lotto:
+    def __init__(self, amount):
+        self.numbers = [random.randint(0,48) for i in range (amount)]
 
+    def check(self, lotto):
+        matching_array = lotto.numbers
+        for number in self.numbers:
+            if number in matching_array:
+                matching_array.remove(number)
+            else:
+                return False
+        return True
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    @classmethod
+    def calc_prob(self, sample_size):
+        succ_tries = 0
+        for i in range(sample_size):
+            l = [random.randint(0,48) for i in range(6)]
+            if 2 in set([l.count(n) for n in l]):
+                succ_tries += 1
+        probability = succ_tries / sample_size
+        return probability
 
+lotto1 = Lotto(6)
+lotto2 = lotto1
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if lotto1.check(lotto2):
+    print("yes")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(f"{Lotto.calc_prob(100000) * 100}%")

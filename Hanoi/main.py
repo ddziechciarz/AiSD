7f58df5ff2271c2ecf7cfe278244b5b5b5c0abd0
Alawon1 <alawon12@gmail.com>
2022-04-28 13:06:44 +0200

    updated hanoi function using recursion and iteration

--- Hanoi/main.py
+++ Hanoi/main.py
@@ -1,27 +1,34 @@
+# def hanoi_rec(size, start, end):
+#     move_count = 0
+#     if size <= 1:
+#         print(f"{start} -> {end}")
+#         return move_count + 1
+#     else:
+#         other = 6 - (start + end)
+#         move_count += hanoi_rec(size - 1, start, other)
+#         print(f"{start} -> {end}")
+#         move_count += 1
+#         move_count += hanoi_rec(size - 1, other, end)
+#         return move_count
 
-
-def hanoi_rec(size, start, end):
+def hanoi_recursion(size, start, buffer, end):
     move_count = 0
     if size <= 1:
-        print(f"{start} -> {end}")
+        end.append(start.pop)
         return move_count + 1
     else:
-        other = 6 - (start + end)
-        move_count += hanoi_rec(size - 1, start, other)
-        print(f"{start} -> {end}")
+        move_count += hanoi_recursion(size - 1, start, end, buffer)
+        end.append(start.pop)
         move_count += 1
-        move_count += hanoi_rec(size - 1, other, end)
+        move_count += hanoi_recursion(size - 1, buffer, start, end)
         return move_count
 
-# def make_move(start_stack, end_stack):
-#     end_stack.append(start_stack.pop)
 
-
-def hanoi_iter(size):
+def hanoi_iter(size, start_stack:list, help_stack:list, end_stack:list, print_moves=False):
     i = 1
-    stack_start = [i + 1 for i in range(size)]
-    stack_help = []
-    stack_end = []
+    stack_start = start_stack
+    stack_help = help_stack
+    stack_end = end_stack
 
     if stack_start[-1] % 2 == 0:
         is_even = True
@@ -33,11 +40,13 @@ def hanoi_iter(size):
             #print(f"start_stack: {stack_start}, end_stack: {stack_end}")
             #possible move between start and end
             if len(stack_start) != 0 and (len(stack_end) == 0 or stack_start[-1] > stack_end[-1]):
-                print("1 -> 3")
+                if print_moves:
+                    print("1 -> 3")
                 i += 1
                 stack_end.append(stack_start.pop())
             else:
-                print("3 -> 1")
+                if print_moves:
+                    print("3 -> 1")
                 stack_start.append(stack_end.pop())
                 i += 1
 
@@ -45,11 +54,13 @@ def hanoi_iter(size):
             #print(f"start_stack: {stack_start}, help_stack: {stack_help}")
             #possible move between start and help
             if len(stack_start) != 0 and (len(stack_help) == 0 or stack_start[-1] > stack_help[-1]):
-                print("1 -> 2")
+                if print_moves:
+                    print("1 -> 2")
                 stack_help.append(stack_start.pop())
                 i += 1
             else:
-                print("2 -> 1")
+                if print_moves:
+                    print("2 -> 1")
                 stack_start.append(stack_help.pop())
                 i += 1
 
@@ -57,18 +68,31 @@ def hanoi_iter(size):
             #print(f"help_stack: {stack_help}, end_stack: {stack_end}")
             #possbile move between help and end
             if len(stack_help) != 0 and (len(stack_end) == 0 or stack_help[-1] > stack_end[-1]):
-                print("2 -> 3")
+                if print_moves:
+                    print("2 -> 3")
                 stack_end.append(stack_help.pop())
                 i += 1
             else:
-                print("3 -> 2")
+                if print_moves:
+                    print("3 -> 2")
                 stack_help.append(stack_end.pop())
                 i += 1
 
     return i - 1
 
 
-iteration_moves = hanoi_iter(15)
-recursion_moves = hanoi_rec(15, 1, 3)
+def calc(sample_size):
+    time_dict = {}
+    for i in range(1, sample_size + 1):
+        print(i)
+        stack_start = [b for b in range(1, i + 1)]
+        stack_buffer = []
+        stack_end = []
+        time_dict[f"{i}"] = (hanoi_iter(i, stack_start, stack_buffer, stack_end),
+                             hanoi_recursion(i, stack_start, stack_buffer, stack_end))
 
-print(iteration_moves, recursion_moves)
+    return time_dict
+if __name__ == "__main__":
+    print(calc(20))
+
+

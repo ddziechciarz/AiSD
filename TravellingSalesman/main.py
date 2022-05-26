2518b542e8b6207143ceb30404a67d36ffb77032
Alawon1 <alawon12@gmail.com>
2022-05-26 12:07:35 +0200

    small adjustments

--- TravellingSalesman/main.py
+++ TravellingSalesman/main.py
@@ -1,5 +1,6 @@
 import math
 
+
 class City:
     def __init__(self, index, x, y):
         self.index = index
@@ -28,29 +29,37 @@ def load_data():
             cities.append(City(int(line[0]), float(line[1]), float(line[2])))
     return cities
 
+
 def fast_sol(city_list: list[City]):
     total_len = 0
     for i in range(1, len(city_list)):
-        total_len += city_list[i].calc_dist(city_list[i-1])
+        total_len += city_list[i].calc_dist(city_list[i - 1])
     total_len += city_list[i].calc_dist(city_list[0])
     return total_len
+
 
 def nearest_neigh(city_list: list[City]):
     current_point = city_list[0]
     route = "1, "
     cities_left = city_list[1:]
     num_of_visited_cities = 0
+    total_len = 0
 
     while num_of_visited_cities < len(city_list) - 1:
         nearest_index = current_point.find_nearest_neigh(cities_left) - 1
-        route += f"{city_list[nearest_index].index }, "
+        total_len += current_point.calc_dist(city_list[nearest_index])
+        route += f"{city_list[nearest_index].index}, "
         num_of_visited_cities += 1
         cities_left[nearest_index - 1] = None
         current_point = city_list[nearest_index]
+
+    total_len += current_point.calc_dist(city_list[0])
+
+    print(total_len)
     return route
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     cities = load_data()
     print(nearest_neigh(cities))
-
+    print(fast_sol(cities))
